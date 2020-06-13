0221df3a430d71410e392b08e03d86042ab7baa1
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2020-06-13 15:08:31 +0200

    Use u32 buffers and fancy rust iterators to draw.

--- src/painter.rs
+++ src/painter.rs
@@ -11,6 +11,7 @@ pub struct Painter {
     offset: f32,
     last_frame: u32,
 }
+
 impl Painter {
     pub fn new(shm: Main<wl_shm::WlShm>) -> Painter {
         Painter {
@@ -24,11 +25,25 @@ impl Painter {
         let height = 400;
         let stride = 4 * width;
         let size = height * stride;
-        let mut buffer = shared_memory::MemMap::anon_file(size as usize)?;
-        self.draw_checkerboard_pattern(&mut buffer, width, height, (self.offset as i32) % 8);
+        let mut buffer = shared_memory::MemMap::anon_file(size)?;
+        let u32_buffer = u8_to_u32_slice(&mut buffer);
+        self.draw_checkerboard_pattern(
+            u32_buffer, 
+            width, 
+            (self.offset as usize) % 8
+        );
 
-        let pool = self.shm.create_pool(buffer.backing_file().as_raw_fd(), size);
-        let wl_buffer = pool.create_buffer(0, width, height, stride, wl_shm::Format::Xrgb8888);
+        let pool = self.shm.create_pool(
+            buffer.backing_file().as_raw_fd(),
+            size as i32
+        );
+        let wl_buffer = pool.create_buffer(
+            0,
+            width as i32,
+            height as i32,
+            stride as i32,
+            wl_shm::Format::Xrgb8888
+        );
         pool.destroy();
         wl_buffer.quick_assign(|buffer, event, _| {
             if let wl_buffer::Event::Release = event {
@@ -38,23 +53,15 @@ impl Painter {
         Ok(wl_buffer.detach())
     }
 
-    fn draw_checkerboard_pattern(&self, buffer: &mut [u8], width: i32, height: i32, offset: i32) {
-        let color1 = (0xFF666666 as u32).to_le_bytes();
-        let color2 = (0xFFEEEEEE as u32).to_le_bytes();
-        let mut index = 0;
-        for y in 0..height {
-            for x in 0..width {
-                let color = 
+    fn draw_checkerboard_pattern(&self, buffer: &mut [u32], width: usize, offset: usize) {
+        for (y, row) in buffer.chunks_exact_mut(width).enumerate() {
+            for (x, pixel) in row.iter_mut().enumerate() {
+                *pixel = 
                     if ((x + offset) + (y + offset) / 8 * 8) % 16 < 8 {
-                        &color1
+                        0xFF666666
                     } else {
-                        &color2
+                        0xFFEEEEEE
                     };
-                buffer[index] = color[0];
-                buffer[index + 1] = color[1];
-                buffer[index + 2] = color[2];
-                buffer[index + 3] = color[3];
-                index += 4;
             }
         }
     }
@@ -65,4 +72,12 @@ impl Painter {
         }
         self.last_frame = time;
     }
+}
+
+
+fn u8_to_u32_slice(x: &mut [u8]) -> &mut [u32] {
+    assert!(x.len() % 4 == 0);
+    let ptr = x.as_mut_ptr();
+    let ptr = unsafe { std::mem::transmute(ptr) };
+    unsafe { std::slice::from_raw_parts_mut(ptr, x.len()/4) }
 }
